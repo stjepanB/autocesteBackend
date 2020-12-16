055521fe87447e6a785c4e038bf7f16602560c60
stjepanB <stjepan.bencic95@gmail.com>
2020-12-16 19:10:07 +0100

    registration validation implementation in progress

--- src/main/java/diplomski/autoceste/controllers/PrivateUserRegistrationController.java
+++ src/main/java/diplomski/autoceste/controllers/PrivateUserRegistrationController.java
@@ -4,44 +4,33 @@ import diplomski.autoceste.formsData.RegisterPrivateUserDto;
 import diplomski.autoceste.model.PrivateUser;
 import diplomski.autoceste.repositories.UserRepository;
 import diplomski.autoceste.security.AutocesteUserRole;
+import diplomski.autoceste.service.PrivateUserRegistrationService;
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
 
 @RestController
 public class PrivateUserRegistrationController {
 
-    private UserRepository repository;
-    private final PasswordEncoder passwordEncoder;
+    private PrivateUserRegistrationService service;
 
     @Autowired
-    public PrivateUserRegistrationController(UserRepository repository, PasswordEncoder passwordEncoder) {
-        this.repository = repository;
-        this.passwordEncoder = passwordEncoder;
+    public PrivateUserRegistrationController(PrivateUserRegistrationService service) {
+        this.service = service;
     }
 
     @PostMapping("/register")
     public HttpStatus register (@RequestBody RegisterPrivateUserDto dto){
 
-        PrivateUser privateUser = new PrivateUser();
-        privateUser.setRole(AutocesteUserRole.USER.name());
-        privateUser.setAccountNonExpired(true);
-        privateUser.setEnabled(true);
-        privateUser.setAccountNonLocked(true);
-        privateUser.setCredentialsNonExpired(true);
-        privateUser.setPassword(passwordEncoder.encode(dto.getPassword()));
-        privateUser.setEmail(dto.getEmail());
-        privateUser.setFirstName(dto.getFirstName());
-        privateUser.setLastName(dto.getLastName());
-        privateUser.setInvalid(dto.isInvalid());
-        privateUser.setAddress(dto.getAddress());
-        privateUser.setOib(dto.getOib());
-
-        repository.save(privateUser);
-        return HttpStatus.OK;
+        return service.register((dto)) ? HttpStatus.OK : HttpStatus.CONFLICT;
     }
 }
