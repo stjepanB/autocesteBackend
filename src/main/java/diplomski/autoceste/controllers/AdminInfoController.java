d5723ad1183627f5fde7c7feaef078b5b801f3ce
stjepanB <stjepan.bencic95@gmail.com>
2021-01-09 17:50:04 +0100

    minor updates

--- src/main/java/diplomski/autoceste/controllers/AdminInfoController.java
+++ src/main/java/diplomski/autoceste/controllers/AdminInfoController.java
@@ -2,12 +2,14 @@ package diplomski.autoceste.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class AdminInfoController {
 
     @GetMapping("/admin")
--- src/main/java/diplomski/autoceste/controllers/HighwaySectionController.java
+++ src/main/java/diplomski/autoceste/controllers/HighwaySectionController.java
@@ -8,16 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class HighwaySectionController {
 
     private final HighwaySectionService highwaySectionService;
